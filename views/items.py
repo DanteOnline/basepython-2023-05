03222521c7a9a78a8211d6b7e65f9da4f71b76ef
DanteOnline <iamdanteonline@gmail.com>
2023-11-07 22:20:49 +0400

    add tests

--- views/items.py
+++ views/items.py
@@ -23,6 +23,10 @@ def get_items_list():
     }
 
 
+def get_item_by_id_logic(item_id):
+    return {"data": {"id": item_id}}
+
+
 @items_app.get("/<int:item_id>/")
 def get_item_by_id(item_id: int):
-    return {"data": {"id": item_id}}
+    return get_item_by_id_logic(item_id)
